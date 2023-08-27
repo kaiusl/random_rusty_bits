630fba098b2083d4b9dc8bff2254ea33a001d572
Kaius Loos <kaius.loos@gmail.com>
2023-08-27 22:01:22 +0300

    sorting: add insertion sort

--- sort/src/lib.rs
+++ sort/src/lib.rs
@@ -12,6 +12,34 @@ pub fn bubble_sort<T: PartialOrd>(slice: &mut [T]) {
     }
 }
 
+pub fn insertion_sort<T>(slice: &mut [T])
+where
+    T: PartialOrd,
+{
+    for j in 1..slice.len() {
+        let to_sort = &slice[j];
+        let mut new_index = 0;
+        for i in (0..j).rev() {
+            if &slice[i] < to_sort {
+                new_index = i + 1;
+                break;
+            }
+        }
+        slice[new_index..=j].rotate_right(1);
+    }
+}
+
+pub fn insertion_sort2<T>(slice: &mut [T])
+where
+    T: PartialOrd,
+{
+    for j in 1..slice.len() {
+        let to_sort = &slice[j];
+        let new_index = slice[..j].partition_point(|a| a < to_sort);
+        slice[new_index..=j].rotate_right(1);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -28,6 +56,20 @@ mod tests {
     fn bubble_sort_test() {
         let mut arr = vec![1, 4, 2, 24, 65, 3, 3, 45];
         bubble_sort(arr.as_mut_slice());
+        assert_sorted(&arr);
+    }
+
+    #[test]
+    fn insertion_sort_test() {
+        let mut arr = vec![1, 4, 2, 24, 65, 3, 3, 45];
+        insertion_sort(arr.as_mut_slice());
+        assert_sorted(&arr);
+    }
+
+    #[test]
+    fn insertion_sort2_test() {
+        let mut arr = vec![1, 4, 2, 24, 65, 3, 3, 45];
+        insertion_sort2(arr.as_mut_slice());
         assert_sorted(&arr);
     }
 
@@ -54,6 +96,22 @@ mod tests {
                 mut vec in proptest::collection::vec(0..10000i32, 0..VEC_SIZE),
             ) {
                bubble_sort(vec.as_mut_slice());
+               assert_sorted(&vec);
+            }
+
+            #[test]
+            fn insertion_sort_test(
+                mut vec in proptest::collection::vec(0..10000i32, 0..VEC_SIZE),
+            ) {
+               insertion_sort(vec.as_mut_slice());
+               assert_sorted(&vec);
+            }
+
+            #[test]
+            fn insertion_sort2_test(
+                mut vec in proptest::collection::vec(0..10000i32, 0..VEC_SIZE),
+            ) {
+               insertion_sort2(vec.as_mut_slice());
                assert_sorted(&vec);
             }
         );
